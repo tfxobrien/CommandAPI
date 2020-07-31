129ec91c828b1fa74d745e51d880badb743c3202
Tim OBrien <tfxobrien@gmail.com>
2020-07-31 15:19:55 -0400

    EFCore added

--- src/CommandAPI/Data/MockCommandAPIRepo.cs
+++ src/CommandAPI/Data/MockCommandAPIRepo.cs
@@ -17,20 +17,20 @@ namespace CommandAPI.Data
     public IEnumerable<Command> GetAllCommands()
     {
       var commands = new List<Command>
- {
- new Command{
- Id=0, HowTo="How to generate a migration",
- CommandLine="dotnet ef migrations add <Name of Migration>",
- Platform=".Net Core EF"},
- new Command{
- Id=1, HowTo="Run Migrations",
- CommandLine="dotnet ef database update",
- Platform=".Net Core EF"},
- new Command{
- Id=2, HowTo="List active migrations",
- CommandLine="dotnet ef migrations list",
- Platform=".Net Core EF"}
- };
+      {
+        new Command{
+          Id=0, HowTo="How to generate a migration",
+          CommandLine="dotnet ef migrations add <Name of Migration>",
+          Platform=".Net Core EF"},
+        new Command{
+          Id=1, HowTo="Run Migrations",
+          CommandLine="dotnet ef database update",
+          Platform=".Net Core EF"},
+        new Command{
+          Id=2, HowTo="List active migrations",
+          CommandLine="dotnet ef migrations list",
+          Platform=".Net Core EF"}
+      };
       return commands;
     }
 
