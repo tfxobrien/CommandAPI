cdd5484463e4ddd2ed25068efc54df47ca36b7b1
Tim OBrien <tfxobrien@gmail.com>
2020-08-31 09:31:03 -0400

    test of pipeline fixed

--- test/CommandAPI.Tests/CommandControllerTests.cs
+++ test/CommandAPI.Tests/CommandControllerTests.cs
@@ -319,7 +319,7 @@ namespace CommandAPI.Tests
       var result = controller.DeleteCommand(0);
 
       //Assert
-      Assert.IsType<OkResult>(result);
+      Assert.IsType<NotFoundResult>(result);
     }
 
     //**************************************************
